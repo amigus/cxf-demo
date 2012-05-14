93afbc5a052fc046aa1edd5b1214cc9126ff106d
Adam C. Migus <adam@migus.org>
2012-05-13 22:58:42 -0400

    Convert from Derby to HQSL for unit tests   - Add HSQL dependency to cxf-webservices, ibatis-data-access and     parent POM files   - Update Spring context files of cxf-webservices and     ibatis-data-access define DataSources using jdbc:embedded-database   - Remove unsed jdbc.properties files   - Move derby configuration from the parent POM to the webservices POM   - Add an M2Eclipse lifecycle-mapping for sql-maven-plugin in webservices

--- ibatis-data-access/src/test/resources/hsqldb-init.sql
+++ ibatis-data-access/src/test/resources/hsqldb-init.sql
@@ -1,9 +1,9 @@
-DROP TABLE Content;
+DROP TABLE Content IF EXISTS
 CREATE TABLE Content(
 	id char(36) not null,
 	author varchar(100) not null,
 	title varchar(100) not null,
-	text varchar(2000) not null,
+	text varchar(32768) not null,
 	created date not null,
 	primary key (id)
 );
